fcd02f9224dd874d7f1f7ea6b40b860ca5a0d648
Aditya Dev <adityadev.india@gmail.com>
2016-11-16 00:42:02 -0800

    Final Solution

--- src/PrintOddNumbersInRange.java
+++ src/PrintOddNumbersInRange.java
@@ -2,7 +2,15 @@
  * Created by adityadev on 11/15/16.
  */
 public class PrintOddNumbersInRange {
+    private static final int MAX_VALUE = 100;
+
     public static void main(String[] args) {
+        int pointer = -1;
+
+        System.out.println("Odd numbers in the Range 0 to 100:");
+        while ((pointer += 2) < MAX_VALUE) {
+            System.out.print(pointer + "\t");
+        }
 
     }
 }
